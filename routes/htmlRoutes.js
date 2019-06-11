e4287a55babb4dd5a364bc848c0bec8824fd8f19
Adrian Quezada <adrianquezada38@gmail.com>
2019-06-10 21:58:12 -0400

    first commit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/home", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
@@ -12,7 +12,8 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  app.get("/donate/:id", function(req, res) {
+    // eslint-disable-next-line prettier/prettier
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
@@ -20,6 +21,16 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/request/:id", function(req, res) {
+    // eslint-disable-next-line prettier/prettier
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+
+  
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
